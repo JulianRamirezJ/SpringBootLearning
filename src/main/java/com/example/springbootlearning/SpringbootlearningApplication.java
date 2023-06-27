85993cb57a9430dd4289f2a7da921bc9c4d2bad9
= <57159295+JulianRamirezJ@users.noreply.github.com>
2023-06-27 16:16:06 -0500

    Student endpoint added

--- src/main/java/com/example/springbootlearning/SpringbootlearningApplication.java
+++ src/main/java/com/example/springbootlearning/SpringbootlearningApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springbootlearning.student.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,10 +19,4 @@ public class SpringbootlearningApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootlearningApplication.class, args);
 	}
-
-	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
-	}
-
 }
